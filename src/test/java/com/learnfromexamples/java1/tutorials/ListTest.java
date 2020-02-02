b4033b83aa3cf36daad199d692c172ddaac248f3
sridhar462 <sridhar462@gmail.com>
2020-02-02 08:57:44 +0530

    added junit

---
+++ src/test/java/com/learnfromexamples/java1/tutorials/ListTest.java
@@ -0,0 +1,13 @@
+package com.learnfromexamples.java1.tutorials;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ListTest {
+	 @Test
+	    public void testMath1() {
+	        assertThat(1 + 1, is(2));
+	    }
+}
